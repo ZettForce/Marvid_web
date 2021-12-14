c6c8d9d8c5e06b65aaa14b5828d7c38313f89a81
ZettForce <filyche@gmail.com>
2021-12-14 12:53:47 -0700

    Agregue eliminar usuario v1

--- app/Http/Livewire/Alumnos/IndexAlumnos.php
+++ app/Http/Livewire/Alumnos/IndexAlumnos.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire\Alumnos;
 
+use App\Models\Alumno;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class IndexAlumnos extends Component
 {
+    use WithPagination;
+    protected $paginationTheme='bootstrap';
+    public $search;
+    public $cargando = false;
     public function render()
     {
-        return view('livewire.alumnos.index-alumnos');
+        $alumnos = ($this->cargando == true) ? Alumno::where('nombre', 'LIKE', '%' .$this->search .'%')
+        ->orwhere ('apellidoPaterno', 'LIKE', '%' .$this->search .'%')
+        ->orwhere ('apellidoMaterno', 'LIKE', '%' .$this->search .'%')
+        ->paginate(10) : [];
+        return view('livewire.alumnos.index-alumnos', compact('alumnos'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Alumnos\CrearAlumnos;
+use App\Http\Livewire\Alumnos\IndexAlumnos;
 use App\Http\Livewire\Login\Login;
 use App\Http\Livewire\Usuarios\BorrarUsuarios;
 use App\Http\Livewire\Usuarios\CrearUsuarios;
@@ -20,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/login',Login::class)->name('login');
 /* Proteger rutas */
-
+/* Rutas protegidas */
 Route::group(['middleware' => 'auth'], function (){
 /*Ruta de usuarios*/
 Route::get('/usuarios',IndexUsuarios::class)->name('index.usuarios');
@@ -28,5 +30,9 @@ Route::get('/usuarios/crear',CrearUsuarios::class)->name('crear.usuarios');
 Route::get('usuarios/{agregar}/mostrar',MostrarUsuarios::class)->name('mostrar.usuarios');
 Route::get('usuarios/{agregar}/editar',EditarUsuarios::class)->name('editar.usuarios');
 Route::get('/usuarios/{agregar}/eliminar', BorrarUsuarios::class)->name('borrar.usuarios');
+
+/*Rutas de alumnos*/
+Route::get('/alumnos',IndexAlumnos::class)->name('index.alumnos');
+Route::get('/alumnos/crear',CrearAlumnos::class)->name('crear.alumnos');
 
 });
